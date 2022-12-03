6c63add61521872d76cd7988f95ce0c7eac49bf9
Jason Delgado <blackhat23507@gmail.com>
2022-12-03 17:44:10 -0600

    Create playRound function

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// debugger;
 // getComputerChoice randomly returns rock, paper, scissors
 function getComputerChoice(){
     let randNum = Math.floor(Math.random() * 3);
@@ -16,4 +17,35 @@ function getComputerChoice(){
             console.log("something went wrong...");
     }
 }
-console.log(getComputerChoice());
+
+// Function play round returns the winner        0: 'Rock' 1: 'Paper' 2: 'Siccors'
+function playRound(playerSel){
+    
+    let CHOICES = ["Rock", "Paper", "Scissors"];
+    let computerChoice = getComputerChoice();
+
+    let playerChoice = playerSel.charAt(0).toUpperCase() + playerSel.substring(1);
+    computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.substring(1);
+
+    if(playerChoice == 'Rock' && computerChoice == 'Scissors'){
+        return `You Win! ${playerChoice} beats ${computerChoice}`;
+    }
+
+    else if (playerChoice == 'Paper' && computerChoice == 'Rock'){
+        return `You Win! ${playerChoice} beats ${computerChoice}`;
+    }
+
+    else if (playerChoice == 'Scissors' && computerChoice == 'Paper'){
+        return `You Win! ${playerChoice} beats ${computerChoice}`;
+    }
+
+    else if(playerChoice == computerChoice){
+        return "It's a Tie!";
+    }
+    
+    else {
+        return `You Lose! ${computerChoice} beats ${playerChoice}`;
+    }
+}
+
+console.log(playRound('scissors'));
